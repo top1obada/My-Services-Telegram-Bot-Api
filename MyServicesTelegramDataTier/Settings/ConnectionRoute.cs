0ff859d90553485013d88149b26e2059f88c06b7
Obada Ajaj <top1obada@gmail.com>
2025-11-18 05:09:01 +0300

    Update ConnectionRoute.cs

--- MyServicesTelegramDataTier/Settings/ConnectionRoute.cs
+++ MyServicesTelegramDataTier/Settings/ConnectionRoute.cs
@@ -8,10 +8,9 @@ namespace MyServicesTelegramDataTier.Settings
 {
     public static class clsConnectionRouteData
     {
-        //Server=.;Database=MyServicesDB;User Id=sa;Password=sa123456;TrustServerCertificate=True;
-        //Server=db31167.public.databaseasp.net; Database=db31167; User Id=db31167; Password=Jg3=s8@Y?N2w; Encrypt=False; MultipleActiveResultSets=True;
+        
         public static string ConnectionString =
-            "Server=db31167.public.databaseasp.net; Database=db31167; User Id=db31167; Password=Jg3=s8@Y?N2w; Encrypt=False; MultipleActiveResultSets=True;";
+            "Server="your connection string here";
 
     }
 }
